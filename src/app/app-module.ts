7e037cf99cbbf378dc0a89fac0bac9671935ca44
IEVN1001-23002512 <romema0920@gmail.com>
2026-09-19 10:00:23 -0600

    Pipes 19-09-2026

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
+import { FormsModule } from '@angular/forms';
+import { HeroesFilterPipe } from './heroes/heroes-filter-pipe';
 
 @NgModule({
-  declarations: [App, HeroesList],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [App, HeroesList, HeroesFilterPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,8 +1,61 @@
 import { Component } from '@angular/core';
+import {IHeroes} from '../heroes';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
   templateUrl: './heroes-list.html',
+  //
 })
-export class HeroesList {}
+export class HeroesList 
+{
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+  
+  heroes:IHeroes[]=[
+  {
+    imagen:'https://dragonball-api.com/characters/goku_normal.webp',
+    nombre:'Goku',
+    description:'Estudiante de Muten Roshi',
+    race:'Saiyan',
+    ki:60000000
+  },
+  {
+    imagen:'https://dragonball-api.com/characters/bulma.webp',
+    nombre:'Bulma',
+    description:'La mejor ingieniera de la Tierra',
+    race:'Human',
+    ki:0
+  },
+  {
+    imagen:'https://dragonball-api.com/characters/roshi.webp',
+    nombre:'Muten Roshi',
+    description:'Maestro del estilo tortuga',
+    race:'Human',
+    ki:500000
+  },
+  {
+    imagen:'https://dragonball-api.com/characters/BuuGordo_Universo7.webp',
+    nombre:'Majin Buu',
+    description:'Mantenido de Mr. Satan',
+    race:'Majin',
+    ki:8000000000000
+  },
+  {
+    imagen:'https://dragonball-api.com/characters/Androide_18_Artwork.webp',
+    nombre:'Androide 18',
+    description:'Creacion de Dr. Gero',
+    race:'Android',
+    ki:280000000
+  }
+]
+
+
+
+}
